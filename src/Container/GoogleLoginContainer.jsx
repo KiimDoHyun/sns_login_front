cec4fc95ae507ef28f500638a920c1f3ffde3868
9thxg <ddr04099@gmail.com>
2022-09-29 13:20:18 +0900

    구글 로그인 api response 데이터 저장하도록 수정

--- src/Container/GoogleLoginContainer.jsx
+++ src/Container/GoogleLoginContainer.jsx
@@ -4,9 +4,12 @@ import { GoogleOAuthProvider, googleLogout } from "@react-oauth/google";
 import { GoogleLoginApi } from "../api/auth";
 import { GOOGLE_KEY } from "../key";
 import GoogleLoginComponent from "../Components/GoogleLoginComponent";
+import { rc_user_userInfo } from "../store/user";
+import { useSetRecoilState } from "recoil";
 
 const GoogleLoginContainer = () => {
     const navigate = useNavigate();
+    const rc_setUser_userInfo = useSetRecoilState(rc_user_userInfo);
 
     const responseGoogle = (response) => {
         console.log(response);
@@ -17,9 +20,8 @@ const GoogleLoginContainer = () => {
             const body = { token: res.credential };
             const response = await GoogleLoginApi(body);
             console.log(`res`, response);
-            if (response.data.type === "success") {
-                navigate("/home");
-            }
+            navigate("/home");
+            rc_setUser_userInfo(response.data);
         } catch (e) {
             console.log(`res error`, res);
         }

c92b0eefc42dab999bb373d0f03d66fbfc68d87f
KiimDoHyun <bzidol@naver.com>
2022-09-29 13:07:08 +0900

    예외처리 추가

--- src/Components/HomeComponent.jsx
+++ src/Components/HomeComponent.jsx
@@ -15,35 +15,37 @@ const HomeComponent = (props) => {
                 <h2>HomeComponent</h2>
                 <Button onClick={onClickLogout}>Logout</Button>
             </div>
-            <div className="body">
-                <div>
-                    <div className="userInfoTable">
-                        <h3>사용자 정보</h3>
-                        <>
-                            <div className="infoBox infoBox_Header">
-                                <div>Key</div>
-                                <div>Value</div>
-                            </div>
-                            <div className="infoBox infoBox_Header">
-                                <div>기본 정보</div>
-                            </div>
-                            <div className="infoBox">
-                                <div>type</div>
-                                <div>{rc_user_userInfoValue.type}</div>
-                            </div>
-                            <div className="infoBox">
-                                <div>message</div>
-                                <div>{rc_user_userInfoValue.message}</div>
-                            </div>
-                        </>
-                        {/* 등록된 사용자 정보 */}
-                        <div className="infoBox infoBox_Header">
-                            <div>백엔드에 등록된 사용자 정보</div>
-                        </div>
-                        {rc_user_userInfoValue && (
+            {rc_user_userInfoValue && (
+                <div className="body">
+                    <div>
+                        <div className="userInfoTable">
+                            <h3>사용자 정보</h3>
                             <>
-                                {Object.keys(rc_user_userInfoValue.data).map(
-                                    (item, idx) => (
+                                <div className="infoBox infoBox_Header">
+                                    <div>Key</div>
+                                    <div>Value</div>
+                                </div>
+                                <div className="infoBox infoBox_Header">
+                                    <div>기본 정보</div>
+                                </div>
+                                <div className="infoBox">
+                                    <div>type</div>
+                                    <div>{rc_user_userInfoValue.type}</div>
+                                </div>
+                                <div className="infoBox">
+                                    <div>message</div>
+                                    <div>{rc_user_userInfoValue.message}</div>
+                                </div>
+                            </>
+                            {/* 등록된 사용자 정보 */}
+                            <div className="infoBox infoBox_Header">
+                                <div>백엔드에 등록된 사용자 정보</div>
+                            </div>
+                            {rc_user_userInfoValue && (
+                                <>
+                                    {Object.keys(
+                                        rc_user_userInfoValue.data
+                                    ).map((item, idx) => (
                                         <div key={idx} className="infoBox">
                                             <div>{item} </div>
                                             <div>
@@ -54,87 +56,99 @@ const HomeComponent = (props) => {
                                                 }
                                             </div>
                                         </div>
-                                    )
-                                )}
-                            </>
-                        )}
-                        {/* 소셜 서비스 사용자 정보 */}
-                        {rc_user_userInfoValue.data.loginType === "kakao" && (
-                            <>
-                                <div className="infoBox infoBox_Header">
-                                    <div>카카오 정보</div>
-                                </div>
-                                <div className="infoBox">
-                                    <div>connected_at </div>
-                                    <div>
-                                        {
-                                            rc_user_userInfoValue.socialData
-                                                .connected_at
-                                        }
+                                    ))}
+                                </>
+                            )}
+                            {/* 소셜 서비스 사용자 정보 */}
+                            {rc_user_userInfoValue.data.loginType ===
+                                "kakao" && (
+                                <>
+                                    <div className="infoBox infoBox_Header">
+                                        <div>카카오 정보</div>
                                     </div>
-                                </div>
-                                <div className="infoBox">
-                                    <div>id </div>
-                                    <div>
-                                        {rc_user_userInfoValue.socialData.id}
-                                    </div>
-                                </div>
-                                {Object.keys(
-                                    rc_user_userInfoValue.socialData
-                                        .kakao_account
-                                ).map(
-                                    (item, idx) =>
-                                        item !== "profile" && (
-                                            <div key={idx} className="infoBox">
-                                                <div>{item} </div>
-                                                <div>
-                                                    {String(
-                                                        rc_user_userInfoValue
-                                                            .socialData
-                                                            .kakao_account[item]
-                                                    )}
-                                                </div>
-                                            </div>
-                                        )
-                                )}
-                                {Object.keys(
-                                    rc_user_userInfoValue.socialData.properties
-                                ).map((item, idx) => (
-                                    <div key={idx} className="infoBox">
-                                        <div>{item} </div>
+                                    <div className="infoBox">
+                                        <div>connected_at </div>
                                         <div>
-                                            {String(
+                                            {
                                                 rc_user_userInfoValue.socialData
-                                                    .properties[item]
-                                            )}
+                                                    .connected_at
+                                            }
                                         </div>
                                     </div>
-                                ))}
-                            </>
-                        )}
-                        {/* 소셜 서비스 사용자 정보 */}
-                        {rc_user_userInfoValue.data.loginType === "google" && (
-                            <>
-                                <div className="infoBox infoBox_Header">
-                                    <div>구글 정보</div>
-                                </div>
-                                {Object.keys(
-                                    rc_user_userInfoValue.socialData
-                                ).map((item, idx) => (
-                                    <div key={idx} className="infoBox">
-                                        <div>{item} </div>
+                                    <div className="infoBox">
+                                        <div>id </div>
                                         <div>
-                                            {String(
-                                                rc_user_userInfoValue
-                                                    .socialData[item]
-                                            )}
+                                            {
+                                                rc_user_userInfoValue.socialData
+                                                    .id
+                                            }
                                         </div>
                                     </div>
-                                ))}
-                            </>
-                        )}
-                    </div>
-                    {/* {rc_user_userInfoValue &&
+                                    {Object.keys(
+                                        rc_user_userInfoValue.socialData
+                                            .kakao_account
+                                    ).map(
+                                        (item, idx) =>
+                                            item !== "profile" && (
+                                                <div
+                                                    key={idx}
+                                                    className="infoBox"
+                                                >
+                                                    <div>{item} </div>
+                                                    <div>
+                                                        {String(
+                                                            rc_user_userInfoValue
+                                                                .socialData
+                                                                .kakao_account[
+                                                                item
+                                                            ]
+                                                        )}
+                                                    </div>
+                                                </div>
+                                            )
+                                    )}
+                                    {Object.keys(
+                                        rc_user_userInfoValue.socialData
+                                            .properties
+                                    ).map((item, idx) => (
+                                        <div key={idx} className="infoBox">
+                                            <div>{item} </div>
+                                            <div>
+                                                {String(
+                                                    rc_user_userInfoValue
+                                                        .socialData.properties[
+                                                        item
+                                                    ]
+                                                )}
+                                            </div>
+                                        </div>
+                                    ))}
+                                </>
+                            )}
+                            {/* 소셜 서비스 사용자 정보 */}
+                            {rc_user_userInfoValue.data.loginType ===
+                                "google" && (
+                                <>
+                                    <div className="infoBox infoBox_Header">
+                                        <div>구글 정보</div>
+                                    </div>
+                                    {Object.keys(
+                                        rc_user_userInfoValue.socialData
+                                    ).map((item, idx) => (
+                                        <div key={idx} className="infoBox">
+                                            <div>{item} </div>
+                                            <div>
+                                                {String(
+                                                    rc_user_userInfoValue
+                                                        .socialData[item]
+                                                )}
+                                            </div>
+                                        </div>
+                                    ))}
+                                </>
+                            )}
+                        </div>
+                        {/* {rc_user_userInfoValue &&
                     rc_user_userInfoValue.data.loginType === "normal" && (
                         <div>
                             {rc_user_userInfoValue
@@ -151,15 +165,16 @@ const HomeComponent = (props) => {
                                 : null}
                         </div>
                     )} */}
+                    </div>
+                    <div className="apiTest">
+                        <h3>Api 통신 테스트</h3>
+                        <Button onClick={onClickRequestApi}>Request Api</Button>
+                        <div>상태: {apiResult.type}</div>
+                        <div>메세지: {apiResult.message}</div>
+                        <div>로그인 유형: {apiResult.data.loginType}</div>
+                    </div>
                 </div>
-                <div className="apiTest">
-                    <h3>Api 통신 테스트</h3>
-                    <Button onClick={onClickRequestApi}>Request Api</Button>
-                    <div>상태: {apiResult.type}</div>
-                    <div>메세지: {apiResult.message}</div>
-                    <div>로그인 유형: {apiResult.data.loginType}</div>
-                </div>
-            </div>
+            )}
         </HomeComponentBlock>
     );
 };
